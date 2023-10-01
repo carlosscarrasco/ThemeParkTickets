ea78cc05098e0fc8c54b573f93046c798f5da4dc
carlosscarrasco <carlosebastiacarrasco@gmail.com>
2023-10-01 22:00:27 +0200

    Initial commit

---
+++ src/ThemeParkTickets.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TicketCost {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double childTicket = 15.5;
+        double adultTicket = 20;
+
+        System.out.println("Welcome to the theme park!");
+        System.out.println("How many ")
+
+    }
+}
